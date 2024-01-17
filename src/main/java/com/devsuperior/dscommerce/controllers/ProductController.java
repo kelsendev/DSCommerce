397d26fc125af5b59a5f647bc2b172da72cbb7cf
Kelsen Tavares <tiadsrussas@gmail.com>
2024-01-17 14:16:43 -0300

    GET Products paged

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,6 +1,11 @@
 package com.devsuperior.dscommerce.controllers;
 
+
+
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,4 +27,9 @@ public class ProductController {
 				
 	}
 
+	@GetMapping
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		return service.findAll(pageable);
+				
+	}
 }
